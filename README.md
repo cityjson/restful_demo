faa8964d3aa1b30a980a5b152f42230fde52c113
Hugo Ledoux <h.ledoux@tudelft.nl>
2020-03-28 10:02:39 +0100

    How to handle the query string in URLs, was easy

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 $ env FLASK_APP=app.py flask run
 ```
 
+In debug mode:
+```bash
+$ env FLASK_APP=app.py FLASK_ENV=development flask run
+```
--- demo/app.py
+++ demo/app.py
@@ -1,12 +1,14 @@
 
-from flask import Flask, escape, url_for, render_template
+from flask import Flask, escape, url_for, render_template, request
 from cjio import cityjson
 import json
 import os
 
+PATHDATASETS = './datasets/'
+JINVALIDFORMAT = {"code": "InvalidParameterValue", "description": "Invalid format"}
 
 def getcm(filename):
-    p = './datasets/' + filename + '.json'
+    p = PATHDATASETS + filename + '.json'
     if os.path.isfile(p) == False:
         return None
     f = open(p)
@@ -15,17 +17,25 @@ def getcm(filename):
 
 app = Flask(__name__)
 
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('404.html'), 404
+@app.route('/', methods=['GET'])
+def root():
+    re = request.args.get('f', None)
+    if re == 'html' or re is None:
+        return render_template("root.html", datasets=os.listdir(PATHDATASETS))
+    elif re == 'json':
+        j = {}
+        j["hugo"] = "ledoux"
+        return j
+    else:
+        return JINVALIDFORMAT
 
-@app.route('/')
-def hello_cityjson():
-    return render_template("overview.html", datasets=os.listdir('./datasets/'))
 
+@app.route('/collections/', methods=['GET'])
+def collections():
+    return render_template("root.html", datasets=os.listdir(PATHDATASETS))
 
-@app.route('/<filename>/download/')
-def cmd_download(filename):
+@app.route('/collections/<filename>/')
+def collection(filename):
     cm = getcm(filename)
     if cm == None:
         return render_template("wrongdataset.html")
@@ -33,44 +43,59 @@ def cmd_download(filename):
         return cm.j
 
 
-@app.route('/<filename>/')
-def cmd_info(filename):
-    cm = getcm(filename)
-    if cm == None:
-        return render_template("wrongdataset.html")
-    else:        
-        i = cm.get_info()
-        return json.loads(i)
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
 
 
-@app.route('/<filename>/subset/random/<number>/')
-def cmd_subset_random(filename, number):
-    cm = getcm(filename)
-    if cm == None:
-        return render_template("wrongdataset.html")
-    else:        
-        cm2 = cm.get_subset_random(number=int(number), exclude=False)
-        return cm2.j
+
+# @app.route('/<filename>/download/')
+# def cmd_download(filename):
+#     cm = getcm(filename)
+#     if cm == None:
+#         return render_template("wrongdataset.html")
+#     else:        
+#         return cm.j
 
 
-@app.route('/<filename>/subset/bbox/<minx>/<miny>/<maxx>/<maxy>/')
-def cmd_subset_bbox(filename, minx, miny, maxx, maxy):
-    cm = getcm(filename)
-    if cm == None:
-        return render_template("wrongdataset.html")
-    else:        
-        cm2 = cm.get_subset_bbox(bbox=mybbox, exclude=False)
-        return cm2.j
+# @app.route('/<filename>/')
+# def cmd_info(filename):
+#     cm = getcm(filename)
+#     if cm == None:
+#         return render_template("wrongdataset.html")
+#     else:        
+#         i = cm.get_info()
+#         return json.loads(i)
 
 
-@app.route('/<filename>/subset/cotype/<thecotype>/')
-def cmd_subset_cotype(filename, thecotype):
-    cm = getcm(filename)
-    if cm == None:
-        return render_template("wrongdataset.html")
-    else:        
-        cm2 = cm.get_subset_cotype(cotype=thecotype, exclude=False)
-        return cm2.j
+# @app.route('/<filename>/subset/random/<number>/')
+# def cmd_subset_random(filename, number):
+#     cm = getcm(filename)
+#     if cm == None:
+#         return render_template("wrongdataset.html")
+#     else:        
+#         cm2 = cm.get_subset_random(number=int(number), exclude=False)
+#         return cm2.j
+
+
+# @app.route('/<filename>/subset/bbox/<minx>/<miny>/<maxx>/<maxy>/')
+# def cmd_subset_bbox(filename, minx, miny, maxx, maxy):
+#     cm = getcm(filename)
+#     if cm == None:
+#         return render_template("wrongdataset.html")
+#     else:        
+#         cm2 = cm.get_subset_bbox(bbox=mybbox, exclude=False)
+#         return cm2.j
+
+
+# @app.route('/<filename>/subset/cotype/<thecotype>/')
+# def cmd_subset_cotype(filename, thecotype):
+#     cm = getcm(filename)
+#     if cm == None:
+#         return render_template("wrongdataset.html")
+#     else:        
+#         cm2 = cm.get_subset_cotype(cotype=thecotype, exclude=False)
+#         return cm2.j
 
 
     
