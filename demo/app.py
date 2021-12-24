1ef456b6bd885f89846eaed6249b5ec41a3f039f
Hugo Ledoux <h.ledoux@tudelft.nl>
2021-12-24 10:11:19 +0100

    Use the CityJSONFeature of cjio directly

--- demo/app.py
+++ demo/app.py
@@ -199,71 +199,10 @@ def collection_stream(dataset):
         return JINVALIDCOLLECTION
     # line-delimited JSON generator
     def generate():    
-        for featureID in cm.j["CityObjects"]:
-            f = cm.get_subset_ids([featureID], exclude=False).j
-            if 'metadata' in f:
-                del f['metadata']
-            if 'version' in f:
-                del f['version']
-            if 'extensions' in f:
-                del f['extensions']
-            f["type"] = "CityJSONFeature"
-            f["id"] = featureID
-            s = json.dumps(f)
-            s += "\n"
-            yield s
+        s = cm.export2jsonl()
+        yield s.getvalue()
     return Response(generate(), mimetype='application/json-seq')
     # return Response(generate(), mimetype='text/plain')
-
-
-
-# @app.route('/<filename>/download/')
-# def cmd_download(filename):
-#     cm = getcm(filename)
-#     if cm == None:
-#         return render_template("wrongdataset.html")
-#     else:        
-#         return cm.j
-
-
-# @app.route('/<filename>/')
-# def cmd_info(filename):
-#     cm = getcm(filename)
-#     if cm == None:
-#         return render_template("wrongdataset.html")
-#     else:        
-#         i = cm.get_info()
-#         return json.loads(i)
-
-
-# @app.route('/<filename>/subset/random/<number>/')
-# def cmd_subset_random(filename, number):
-#     cm = getcm(filename)
-#     if cm == None:
-#         return render_template("wrongdataset.html")
-#     else:        
-#         cm2 = cm.get_subset_random(number=int(number), exclude=False)
-#         return cm2.j
-
-
-# @app.route('/<filename>/subset/bbox/<minx>/<miny>/<maxx>/<maxy>/')
-# def cmd_subset_bbox(filename, minx, miny, maxx, maxy):
-#     cm = getcm(filename)
-#     if cm == None:
-#         return render_template("wrongdataset.html")
-#     else:        
-#         cm2 = cm.get_subset_bbox(bbox=mybbox, exclude=False)
-#         return cm2.j
-
-
-# @app.route('/<filename>/subset/cotype/<thecotype>/')
-# def cmd_subset_cotype(filename, thecotype):
-#     cm = getcm(filename)
-#     if cm == None:
-#         return render_template("wrongdataset.html")
-#     else:        
-#         cm2 = cm.get_subset_cotype(cotype=thecotype, exclude=False)
-#         return cm2.j
 
 
     
