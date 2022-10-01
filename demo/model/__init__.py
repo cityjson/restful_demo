e82873fba5e2e938fe6a019763f25d3c05f1e9f3
Cynthia Cai <81803109+cynthiacai56@users.noreply.github.com>
2022-10-01 13:25:09 +0200

    Create __init__.py

---
+++ demo/model/__init__.py
@@ -0,0 +1,42 @@
+# Author: Leon Powalka
+# Link: https://github.com/leoleonsio/cjdb/blob/main/model/sqlalchemy_models/__init__.py
+
+from sqlalchemy.ext.declarative import declared_attr, declarative_base
+from sqlalchemy import Column, ForeignKey, \
+    String, func, Integer
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import JSONB, TIMESTAMP
+from geoalchemy2 import Geometry
+
+Base = declarative_base()
+
+class BaseModel(Base):
+    __abstract__ = True
+    id = Column(Integer, primary_key=True)
+
+
+class ImportMetaModel(BaseModel):
+    __tablename__ = 'import_meta'
+    __table_args__ = {'schema':'cjdb'}
+    source_file = Column(String)
+    version = Column(String(10), nullable=False)
+    transform = Column(JSONB)
+    meta = Column(JSONB, name="metadata")
+    started_at = Column(TIMESTAMP, default=func.now())
+    finished_at = Column(TIMESTAMP)
+    bbox = Column(Geometry('POLYGON'))
+
+
+class CjObjectModel(BaseModel):
+    __tablename__ = 'cj_object'
+    __table_args__ = {'schema':'cjdb'}
+    import_meta_id = Column(Integer, ForeignKey(ImportMetaModel.id))
+    object_id = Column(String, nullable=False, unique=True)
+    type = Column(String, nullable=False)
+    attributes = Column(JSONB)
+    geometry = Column(JSONB)
+    parents = Column(JSONB)
+    children = Column(JSONB)
+    bbox = Column(Geometry('POLYGON'))
+
+    import_meta = relationship(ImportMetaModel)
